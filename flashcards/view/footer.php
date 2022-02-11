3fc33eb17746a7310a1741264e5f1df41481adb8
Ike Bletz-Fuller <ibletz@users.noreply.github.com>
2022-02-11 14:27:08 -0800

    Add files via upload
    multiplication flashcards app with sass and php

---
+++ flashcards/view/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <?php if ($number) { ?>
+        <a href="index.php">Click Here To Pick Another Number</a>
+    <?php } else { ?>
+        🚀🚀🚀🚀🚀🚀
+    <?php } ?>
+</footer>
